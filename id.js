b4f47258065177317d6872cc5680e5d6f5cd45a3
vanyaportos-dev <vanyaportos@gmail.com>
2026-08-25 19:06:08 +0300

    Add files via upload

---
+++ id.js
@@ -0,0 +1,7 @@
+function genId(prefix = 'id') {
+  const rand = Math.random().toString(36).slice(2, 10);
+  const time = Date.now().toString(36);
+  return `${prefix}_${time}${rand}`;
+}
+
+module.exports = { genId };
